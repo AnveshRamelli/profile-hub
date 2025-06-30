1fc8b0c5abba52311e95ca205dcf16598e79623d
Anvesh Ramelli <anveshramelli1999@gmail.com>
2025-06-30 21:07:29 +0530

    init: nextjs 15 project with tailwind and shadcn-ui

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button className="m-4 px-15 py-5 text-md">Next</Button>
+    </div>
+  );
+}
+
+export default Home;
